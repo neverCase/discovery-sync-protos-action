2ed1d5843898ba9d0175de5b0fb85f561989f393
nevercase <1024769485@qq.com>
2021-08-27 14:30:12 +0800

    import action

---
+++ entrypoint.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+
+set -e
+set -x
+
+if [ -z "$INPUT_SOURCE_FOLDER" ]
+then
+  echo "Source folder must be defined"
+  return -1
+fi
+
+if [ -z "$INPUT_DESTINATION_BRANCH" ]
+then
+  INPUT_DESTINATION_BRANCH=master
+fi
+OUTPUT_BRANCH="$INPUT_DESTINATION_BRANCH"
+
+if [ -z "$INPUT_COMMIT_MSG" ]
+then
+  INPUT_COMMIT_MSG="Update $INPUT_DESTINATION_FOLDER."
+fi
+
+CLONE_DIR=$(mktemp -d)
+
+echo "Cloning destination git repository"
+git config --global user.email "$INPUT_USER_EMAIL"
+git config --global user.name "$INPUT_USER_NAME"
+git clone --single-branch --branch $INPUT_DESTINATION_BRANCH "https://$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
+
+if [ ! -z "$INPUT_DESTINATION_BRANCH_CREATE" ]
+then
+  git checkout -b "$INPUT_DESTINATION_BRANCH_CREATE"
+  OUTPUT_BRANCH="$INPUT_DESTINATION_BRANCH_CREATE"
+fi
+
+echo "Copying contents to git repo"
+#rm -rf $CLONE_DIR/$INPUT_DESTINATION_FOLDER/
+#mkdir -p $CLONE_DIR/$INPUT_DESTINATION_FOLDER/
+#cp -a $INPUT_SOURCE_FOLDER/. $CLONE_DIR/$INPUT_DESTINATION_FOLDER/
+cp $INPUT_SOURCE_FOLDER/apps/v1/generated.proto $CLONE_DIR/$INPUT_DESTINATION_FOLDER/github.com.nevercase.discovery.pkg.apis.apps.v1.proto
+sed -i 's/Shanghai-Lunara/nevercase/g' $CLONE_DIR/$INPUT_DESTINATION_FOLDER/github.com.nevercase.discovery.pkg.apis.apps.v1.proto
+sed -i 's/Shanghai_Lunara/nevercase/g' $CLONE_DIR/$INPUT_DESTINATION_FOLDER/github.com.nevercase.discovery.pkg.apis.apps.v1.proto
+
+cp $INPUT_SOURCE_FOLDER/core/v1/generated.proto $CLONE_DIR/$INPUT_DESTINATION_FOLDER/github.com.nevercase.discovery.pkg.apis.core.v1.proto
+sed -i 's/Shanghai-Lunara/nevercase/g' $CLONE_DIR/$INPUT_DESTINATION_FOLDER/github.com.nevercase.discovery.pkg.apis.core.v1.proto
+sed -i 's/Shanghai_Lunara/nevercase/g' $CLONE_DIR/$INPUT_DESTINATION_FOLDER/github.com.nevercase.discovery.pkg.apis.core.v1.proto
+
+cp $INPUT_SOURCE_FOLDER/helix2/v1/generated.proto $CLONE_DIR/$INPUT_DESTINATION_FOLDER/github.com.nevercase.discovery.pkg.apis.helix2.v1.proto
+sed -i 's/Shanghai-Lunara/nevercase/g' $CLONE_DIR/$INPUT_DESTINATION_FOLDER/github.com.nevercase.discovery.pkg.apis.helix2.v1.proto
+sed -i 's/Shanghai_Lunara/nevercase/g' $CLONE_DIR/$INPUT_DESTINATION_FOLDER/github.com.nevercase.discovery.pkg.apis.helix2.v1.proto
+
+#cp $INPUT_SOURCE_FOLDER/apps/v1/generated.proto $CLONE_DIR/$INPUT_DESTINATION_FOLDER/github.com.nevercase.discovery.pkg.apis.hamster.v1.proto
+#sed -i 's/Shanghai-Lunara/nevercase/g' $CLONE_DIR/$INPUT_DESTINATION_FOLDER/github.com.nevercase.discovery.pkg.apis.hamster.v1.proto
+#sed -i 's/Shanghai_Lunara/nevercase/g' $CLONE_DIR/$INPUT_DESTINATION_FOLDER/github.com.nevercase.discovery.pkg.apis.hamster.v1.proto
+cd "$CLONE_DIR"
+
+echo "Adding git commit"
+git add .
+if git status | grep -q "Changes to be committed"
+then
+  git commit --message "$INPUT_COMMIT_MSG"
+  echo "Pushing git commit"
+  git push -u origin HEAD:$OUTPUT_BRANCH
+else
+  echo "No changes detected"
+fi
